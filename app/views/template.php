606ae84d0290e0027c3296823515370ef1afa182
Lario dos Santos Diniz <lariodiniz@gmail.com>
2016-04-02 20:48:41 -0300

    adicionado favcon

--- app/views/template.php
+++ app/views/template.php
@@ -9,6 +9,9 @@
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+	<link rel="shortcut icon" href="app/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="app/assets/img/favicon.ico" type="image/x-icon">
    
     <title>GEBI 2015175 | <?php echo $title; ?></title>
 
