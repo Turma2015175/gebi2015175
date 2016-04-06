6764911229dedb5be8b9acd99b6e9a7a6cc5d9eb
WPelicioni <wendelpelicioni@gmail.com>
2016-04-06 00:08:11 -0300

    Views
    Livro

---
+++ app/views/livro.php
@@ -0,0 +1,23 @@
+<div class="container marketing">
+
+      <div class="row">
+        <div class="col-md-7">
+          <img class="img-circle" src="C:/wamp/www/gebi2015175/app/assets/img/livro1destaque.jpg" 
+		  alt="Generic placeholder image" >
+		</div>
+		
+        <div class="col-md-5">
+          <h2>As Leis Invisíveis do Dinheiro</h2>
+			<h3>Melo, Ricardo</h3>			
+			<h4>Gente</h4>			     
+			<p>Todos têm o desejo de enriquecer para de alguma forma 
+			melhorar suas vidas, mas o fazem desconectados de suas 
+			emoções e necessidades, pois muitas pessoas não enxergam a 
+			relação entre o desenvolvimento financeiro e o espiritual. 
+			Isso é o que tem tornado o mundo mais materialista a cada dia...</p>
+          <p><a class="btn btn-default" href="#" role="button">Proximo livro</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Livro anterior</a></p>
+        </div>
+        </div>  
+
+    </div>
