7525cbb391dee11d781131714b3dbee3d2a0e696
Luiz Henrique Cordeiro <luizh.cordeiro@yahoo.com.br>
2016-04-08 00:46:02 -0300

    Css inicial livro/livros
    iz alguns acertos e modificações no css de livro e de livros

--- app/views/livro.php
+++ app/views/livro.php
@@ -1,13 +1,10 @@
-<div class="container marketing">
-
-      <div class="row">
-        <div class="col-md-7">
-          <img class="img-circle" src="C:/wamp/www/gebi2015175/app/assets/img/livro1destaque.jpg" 
-		  alt="Generic placeholder image" >
+	<div class="row">
+		<div class="col-md-5">
+		  <img src="app/assets/img/livro1destaque.jpg" alt="Generic placeholder image" >
 		</div>
-		
-        <div class="col-md-5">
-          <h2>As Leis Invisíveis do Dinheiro</h2>
+
+		<div class="col-md-7">
+		  <h2>As Leis Invisíveis do Dinheiro</h2>
 			<h3>Melo, Ricardo</h3>			
 			<h4>Gente</h4>			     
 			<p>Todos têm o desejo de enriquecer para de alguma forma 
@@ -15,9 +12,12 @@
 			emoções e necessidades, pois muitas pessoas não enxergam a 
 			relação entre o desenvolvimento financeiro e o espiritual. 
 			Isso é o que tem tornado o mundo mais materialista a cada dia...</p>
-          <p><a class="btn btn-default" href="#" role="button">Proximo livro</a></p>
-          <p><a class="btn btn-default" href="#" role="button">Livro anterior</a></p>
-        </div>
-        </div>  
-
-    </div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="btn-group" role="group" aria-label="...">
+			  <button type="button" class="btn btn-default">Proximo livro</button>
+			  <button type="button" class="btn btn-default">Livro anterior</button>
+		</div>
+	</div>
+	
--- app/views/livros.php
+++ app/views/livros.php
@@ -4,68 +4,64 @@
 					3 - grupo onde são apresentados livros novos, ou livros relacionado a leitura atual, etc
 					tudo é só uma ideia do que pode ser feito-->
 
-<div class="col-xs-10 col-xs-offset-1">
 		<section>
 		<h1 class="label label-warning">Lista de livros</h1>
 		<img src="app/assets/img/imagemprincipal.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
 		</section>
 
-		<section>
-
+		<section class="livros-secao">
 			<h2>Livros em destaque (pode ser conforme perfil do usuario)</h2>
-			
-			<h3>As Leis Invisíveis do Dinheiro</h3>			<!-- titulo do livro -->
-			<h4>Melo, Ricardo</h4>			<!-- autor do livro -->		
-			<h4>Gente</h4>			<!-- editora do livro -->
-			<img src="app/assets/img/livro1destaque.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
-			<p>Todos têm o desejo de enriquecer para de alguma forma 
-			melhorar suas vidas, mas o fazem desconectados de suas 
-			emoções e necessidades, pois muitas pessoas não enxergam a 
-			relação entre o desenvolvimento financeiro e o espiritual. 
-			Isso é o que tem tornado o mundo mais materialista a cada dia...</p>
-			<br/>
+			<div class="col-md-4">
+				<h3>As Leis Invisíveis do Dinheiro </h3>			<!-- titulo do livro -->
+				<h4>Melo, Ricardo</h4>			<!-- autor do livro -->		
+				<h4>Gente</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro1destaque.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
+				
+			</div>
 			<!-- usei livros aleatorios -->
+			<div class="col-md-4">
+				<h3>O Poder do Hábito - Por Que Fazemos o Que Fazemos na Vida e Nos Negócios</h3>			<!-- titulo do livro -->
+				<h4>Duhigg, Charles</h4>			<!-- autor do livro -->		
+				<h4>Objetiva</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro2destaque.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
+				
+			</div>
+			<div class="col-md-4">
+				<h3>Seja o Empresário da Sua Idéia</h3>			<!-- titulo do livro -->
+				<h4>Oliveira, Natanael</h4>			<!-- autor do livro -->			
+				<h4>Gente</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro2maislidos.jpg" alt="" title="" />
 			
-			<h3>O Poder do Hábito - Por Que Fazemos o Que Fazemos na Vida e Nos Negócios</h3>			<!-- titulo do livro -->
-			<h4>Duhigg, Charles</h4>			<!-- autor do livro -->		
-			<h4>Objetiva</h4>			<!-- editora do livro -->
-			<img src="app/assets/img/livro2destaque.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
-			<p>Durante os últimos dois anos, uma jovem transformou quase 
-			todos os aspectos de sua vida. Parou de fumar, correu uma 
-			maratona e foi promovida. Em um laboratório, neurologistas 
-			descobriram que os padrões dentro do cérebro dela – ou seja, 
-			seus hábitos – foram modificados de maneira fundamental para 
-			que todas essas...</p>
-			<br/>
+			</div>
 			<!-- usei livros aleatorios -->
 		</section>
 		
-		
+		<br/>
 		
 		<section>
 			<h2>Livros mais lidos (pode ser quem leu aquele tambem leu esse, etc)</h2>
-
-			<h3>Geração de Valor – Compartilhando Inspiração</h3>			<!-- titulo do livro -->
-			<h4>Silva, Flávio Augusto Da</h4>			<!-- autor do livro -->			
-			<h4>Sextante</h4>			<!-- editora do livro -->
-			<img src="app/assets/img/livro1maislidos.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
-			<p>Desde que nascem, as pessoas são treinadas para agir de 
-			acordo com o senso comum. O ensino convencional as estimula 
-			a buscar segurança, e não liberdade. Com medo de se 
-			arriscar, a maioria segue o fluxo da boiada e sonha pequeno,
-			optando por conseguir um emprego estável e passar anos 
-			financiando a casa própria...</p>
-			<br/>
+			<div class="col-md-4">
+				<h3>Geração de Valor – Compartilhando Inspiração</h3>			<!-- titulo do livro -->
+				<h4>Silva, Flávio Augusto Da</h4>			<!-- autor do livro -->			
+				<h4>Sextante</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro1maislidos.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
+				
+			</div>
 						<!-- usei livros aleatorios -->
-
-			<h3>Seja o Empresário da Sua Idéia</h3>			<!-- titulo do livro -->
-			<h4>Oliveira, Natanael</h4>			<!-- autor do livro -->			
-			<h4>Gente</h4>			<!-- editora do livro -->
-			<img src="app/assets/img/livro2maislidos.jpg" alt="" title="" />
-			<p>Você provavelmente conhece alguém que esteja 
-			insatisfeito com o trabalho: um parente, um amigo ou até 
-			mesmo alguém que está no seu escritório agora...</p>
+			<div class="col-md-4">
+				<h3>Seja o Empresário da Sua Idéia</h3>			<!-- titulo do livro -->
+				<h4>Oliveira, Natanael</h4>			<!-- autor do livro -->			
+				<h4>Gente</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro2maislidos.jpg" alt="" title="" />
+			
+			</div>
 						<!-- usei livros aleatorios -->
+			<div class="col-md-4">
+				<h3>O Poder do Hábito - Por Que Fazemos o Que Fazemos na Vida e Nos Negócios</h3>			<!-- titulo do livro -->
+				<h4>Duhigg, Charles</h4>			<!-- autor do livro -->		
+				<h4>Objetiva</h4>			<!-- editora do livro -->
+				<img src="app/assets/img/livro2destaque.jpg" alt="" title="" />			<!-- link para apresentar imagens -->
+				
+			</div>
 		</section>
-
-</div>		
+	
