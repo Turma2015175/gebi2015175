6180cc596950de0358ae4ce3339103b8b723beb1
Luiz Henrique Cordeiro <luizh.cordeiro@yahoo.com.br>
2016-04-07 21:20:01 -0300

    Pequenos ajustes
    quem somos

--- app/views/quemsomos.php
+++ app/views/quemsomos.php
@@ -1,165 +1,171 @@
 
-      <hr class="featurette-divider">
-
-      <h2>Nossa Equipe</h2>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Lorenna <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lorenna.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Roberta <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/roberta.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Fernando <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/fernando.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Francisco<span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/francisco.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Ícaro <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/icaro.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Lário <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lario.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Lucas <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lucas.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Luiz <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/luiz.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Marcelo <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/marcelo.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Rafael <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/rafael.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Robson <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/robson.jpg" alt="">
-        </div>
-      </div>
-	  
 	<hr class="featurette-divider">
-	
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Rômulo <span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/romulo.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
-<hr class="featurette-divider">
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Vítor<span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5 ">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/vitor.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
 
-      <hr class="featurette-divider">
+	<h2 style="text-align:center">Nossa Equipe</h2>
 
-      <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-          <h2 class="featurette-heading">Wendel<span class="text-muted">frase inpiradora</span></h2>
-          <p class="lead"></p>
-        </div>
-        <div class="col-md-5  col-md-pull-7">
-          <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/wendel.jpg" alt="Generic placeholder image">
-        </div>
-      </div>
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+			<h2 class="featurette-heading">Lorenna <span class="text-muted">frase inpiradora</span></h2>
+			<p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+			<img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lorenna.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Roberta <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/roberta.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Fernando <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/fernando.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Francisco<span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/francisco.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Ícaro <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/icaro.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Lário <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lario.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Lucas <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/lucas.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Luiz <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/luiz.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Marcelo <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/marcelo.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Rafael <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/rafael.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Robson <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/robson.jpg" alt="">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Rômulo <span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/romulo.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7">
+		  <h2 class="featurette-heading">Vítor<span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5 ">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/vitor.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+	<div class="row featurette">
+		<div class="col-md-7 col-md-push-5">
+		  <h2 class="featurette-heading">Wendel<span class="text-muted">frase inpiradora</span></h2>
+		  <p class="lead"></p>
+		</div>
+		<div class="col-md-5  col-md-pull-7">
+		  <img class="featurette-image img-responsive center-block img-circle" src="app/assets/img/wendel.jpg" alt="Generic placeholder image">
+		</div>
+	</div>
 
