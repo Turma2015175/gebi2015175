753c0e25c108cef5de5169a98b9fd42a4b7f344b
Lario dos Santos Diniz <lariodiniz@gmail.com>
2016-04-11 21:29:57 -0300

    Atualizando View Usuarios

--- app/views/usuarios.php
+++ app/views/usuarios.php
@@ -1,15 +1,83 @@
-<section>
-	<div> Foto do usuario</div>
-	<div> Nome do Usuário</div>
+<section  class ="row">
+	<div class="col-xs-2 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="app/assets/img/usuario01.jpeg" alt="nomedousuario">
+    </a>
+  </div>
+	<div>Victor Hugo</div>
 	<div> id </div>
 </section>
 <section>
 	<h3>Livros Sugeridos</h3>
-	
-	<ul class="bxslider">
-  <li><img src="/images/730_200/tree_root.jpg" title="Funky roots" /></li>
-  <li><img src="/images/730_200/hill_road.jpg" title="The long and winding road" /></li>
-  <li><img src="/images/730_200/trees.jpg" title="Happy trees" /></li>
-</ul>
+    
 
+
+        <div class="col-xs-12 col-sm-9">
+		<div class ="row">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div data-ride="carousel" class="carousel slide" id="myCarousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li class="" data-slide-to="0" data-target="#myCarousel"></li>
+        <li data-slide-to="1" data-target="#myCarousel" class=""></li>
+        <li data-slide-to="2" data-target="#myCarousel" class="active"></li>
+      </ol>
+      <div role="listbox" class="carousel-inner">
+        <div class="item">
+          <img alt="First slide" src="app/assets/img/livro1destaque.jpg" class="first-slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Titulo 1</h1>
+              <p>Descrição do Livro 1</p>
+              <p><a role="button" href="#" class="btn btn-lg btn-primary">Pegar Emprestado</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img alt="Second slide" src="app/assets/img/livro1maislidos.jpg" class="second-slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Titulo 2</h1>
+              <p>Descrição do Livro 2</p>
+              <p><a role="button" href="#" class="btn btn-lg btn-primary">Pegar Emprestado</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item active">
+          <img alt="Third slide" src="app/assets/img/livro2destaque.jpg" class="third-slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Titulo 3</h1>
+              <p>Descrição do Livro 3</p>
+              <p><a role="button" href="#" class="btn btn-lg btn-primary">Pegar Emprestado</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a data-slide="prev" role="button" href="#myCarousel" class="left carousel-control">
+        <span aria-hidden="true" class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a data-slide="next" role="button" href="#myCarousel" class="right carousel-control">
+        <span aria-hidden="true" class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+          
+           
+          </div><!--/row-->
+        </div><!--/.col-xs-12.col-sm-9-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">Aventura</a>
+            <a href="#" class="list-group-item">Ficção</a>
+            <a href="#" class="list-group-item">Terror</a>
+
+          </div>
+        </div><!--/.sidebar-offcanvas-->
+      
 </section>
