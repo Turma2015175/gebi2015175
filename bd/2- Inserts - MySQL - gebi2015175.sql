fd13c812c7ec488bb3355a118a3f149eb113de78
lucasmpbarga <lucasmpbarga@gmail.com>
2016-04-14 22:47:16 -0300

    up2
    up2

---
+++ bd/2- Inserts - MySQL - gebi2015175.sql
@@ -0,0 +1,17 @@
+USE scrinia;
+
+insert into usuario VALUES (null, sha1('123456'), 'Diego Santos', 'diegosantos@user.com', "avatar.jpg");
+insert into usuario VALUES (null, sha1('123456'), 'Danilo Melo', 'danilomelo@user.com', "avatar.jpg");
+insert into usuario VALUES (null, sha1('123456'), 'Bruna Almeida', 'brunaalmeida@user.com', "avatar.jpg");
+
+insert into telefone VALUES (null, '021', '2288-3901', '99913-5608');
+insert into telefone VALUES (null, '022', '2081-4955', '99360-0916');
+insert into telefone VALUES (null, '023', '5754-1607', '99748-1428');
+
+insert into sexo VALUES (null,'Masculino');
+insert into sexo VALUES (null, 'Feminino');
+
+insert into telefone VALUES (null, '023', '5751-7133', '99787-7631');
+
+
+
