55222ac27e033f8727751ebdd0d914ee95d814f1
WPelicioni <wendelpelicioni@gmail.com>
2016-04-18 15:14:09 -0300

    Atualizações
    Atualizações

--- app/views/emprestimo.php
+++ app/views/emprestimo.php
@@ -1,44 +1,51 @@
-<div class="col-xs-10 col-sm-10">
+<div class="col-xs-12">
 
-<h1>Emprestimo</h1>
+<legend>Emprestimo</legend>
 
-	<div class="row">
-		
-		<div class="col-xs-12 col-sm-8">
-			<div class="form-group">
+	<div class="row ">
+
+		<div class="col-xs-12 col-sm-12 col-lg-6 col-md-6">
 				<label class="emprestimo" for="livro">Nome do livro:</label>
 				<input class="form-control" type="text" id="livro" name="livro" placeholder="Digite o nome"/>
-			</div>
 		</div>
-
-		<div class="col-xs-12 col-sm-2">
-			<div class="form-group">
-				<label for="datasaida">Data de emprestimo: </label>
-						<input class="form-control" id="datepicker1" type="text" id="datasaida" name="datasaida"  placeholder="__/__/____">
-				</div>
-			</div>
 		
-		<div class="col-xs-12 col-sm-2">
-			<div class="form-group">
-				<label for="datadevolucao">Data de devolução: </label>
-				<input class="form-control" type="text" id="datadevolucao" name="datadevolucao"  placeholder="__/__/____" disabled>
-			</div>
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 ">
+				<label for="datasaida">Data de emprestimo: </label>
+				<input class="form-control" id="datepicker1" type="text" name="datasaida" onkeypress="mascara(this, '##/##/####')" maxlength="10"  placeholder="__/__/____">
+		</div>	
+		
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 ">
+				<label for="datasaida">Data de emprestimo: </label>
+				<input class="form-control" id="datepicker1" type="text" name="datasaida" onkeypress="mascara(this, '##/##/####')" maxlength="10"  placeholder="__/__/____">
 		</div>
+	</div>
+
+
+	<div class="row">
+
+			<div class="col-xs-12 col-sm-12">
+					<label class="emprestimo" for="enderecobiblioteca">Endereço da biblioteca</label>
+					<input class="form-control" type="text" id="enderecobiblioteca" placeholder="Digite o endereço" name="enderecobiblioteca"/>
+			</div>
 		
 	</div> 
 
-	<form id="formemprestimo">			
-		<div class="row">
-			<div class="col-xs-12 col-sm-12">
-				<div class="form-group">
-					<label class="emprestimo" for="enderecobiblioteca">Endereço da biblioteca</label>
-					<input class="form-control" type="text" id="enderecobiblioteca" name="enderecobiblioteca"/>
-				</div>
-			</div>
-			
+	<div class="row">			
 			<div class="col-xs-12 col-sm-8">
-			<input class="btn btn-default" type="submit" value="confirmar emprestimo" id="enviar"/>
-	
-		</div>
-	</form>
+			<input class="btn btn-default" type="submit" value="Confirmar emprestimo" id="enviar"/>
+			</div>
+	</div>
+
+
 </div>
+
+
+
+
+
+
+
+
+
+
+
